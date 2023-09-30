028d655dfde0f85bd742187a5b04c4ba20430d8a
Lewy Macknold <lewy@Lewys-MacBook.local>
2023-09-30 21:52:43 +0100

    Finished Frontend Website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Analytics from './components/Analytics';
+import Newsletter from './components/Newsletter';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
