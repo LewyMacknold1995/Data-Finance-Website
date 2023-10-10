6340ec653ea9ec6a4df73984a94af17128d6e7cf
Lewy Macknold <lewy@Lewys-MacBook.local>
2023-10-10 14:11:24 +0100

    added hosting fixes

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -10,7 +10,7 @@ const Newsletter = () => {
     </div>
         <div className='my-4'>
             <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
-                <input className='p-3 flex rounded-md text-black' type='email' placeholder='Enter Eamil' />
+                <input className='p-3 flex rounded-md text-black' type='email' placeholder='Enter Email' />
                 <button className='bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3'>Notify Me</button>
              
             </div>
